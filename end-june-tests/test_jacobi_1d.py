01178f6a129e449063ecd72de57d537fa7e23122
Tong Zhou <tz@gatech.edu>
2023-06-24 15:44:13 -0400

    updated test_jacobi_1d

--- end-june-tests/test_jacobi_1d.py
+++ end-june-tests/test_jacobi_1d.py
@@ -4,18 +4,18 @@ import triton.language as tl
 import slap
 from torch import arange, zeros, empty, sum
 
-def slap_kernel(a, N, BLOCK):
-    b = torch.zeros(N-2, device=a.device, dtype=a.dtype)
-    _slap_kernel(a, b, N, BLOCK)
+def slap_kernel(a, N):
+    b = torch.empty(N-2, device=a.device, dtype=a.dtype)
+    _slap_kernel(a, b, N)
     return b
 
 @slap.jit
-def _slap_kernel(a, b, N, BLOCK):
+def _slap_kernel(a, b, N, BLOCK=512):
     for i in range(1, N-1, BLOCK):  #pragma parallel
         i = range(i, i+BLOCK)
         b[i-1] = (a[i-1] + a[i] + a[i+1]) / 3
 
-def torch_kernel(a, N, BLOCK):
+def torch_kernel(a, N):
     b = (a[0:N-2] + a[1:N-1] + a[2:N]) / 3
     return b
     
@@ -24,11 +24,13 @@ def test1():
         N = shape
         print(f'N: {N}')
         a = torch.randn(N, device='cuda', dtype=torch.float32)
-        b_ref = torch_kernel(a, N, None)
+        b_ref = torch_kernel(a, N)
 
         for f in (torch_kernel, slap_kernel):
-            BLOCK = 128 * 4
-            b = f(a, N, BLOCK)
+            b = f(a, N)
             assert(torch.allclose(b, b_ref, atol=1e-2))
-            ms, _, _ = triton.testing.do_bench(lambda: f(a, N, BLOCK))
+            ms, _, _ = triton.testing.do_bench(lambda: f(a, N))
             print(f'{f.__name__}: {ms:.4f} ms')
+
+if __name__ == '__main__':
+    test1()
