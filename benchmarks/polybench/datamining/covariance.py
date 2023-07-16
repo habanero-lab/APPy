f0999cd158c7820848e2d77553cf386a49e34ea6
Tong Zhou <tz@gatech.edu>
2023-07-16 10:58:06 -0400

    updated covariance.py, appy is about 20x faster than numba for 1024x1024 input

--- benchmarks/polybench/datamining/covariance.py
+++ benchmarks/polybench/datamining/covariance.py
@@ -9,94 +9,91 @@ torch.set_default_device('cuda')
 
 def mykernel(a, inner):
     n_vars, n_obs = a.shape
-    u = torch.empty([n_vars], dtype=a.dtype)
+    new_a = torch.empty_like(a)
     b = torch.empty([n_vars, n_vars], dtype=a.dtype)
-    inner(a, b, u, n_vars, n_obs)
+    inner(a, new_a, b, n_vars, n_obs)
     return b
 
-@jit
-def _mykernel(a, b, u, M, N, BN=512):
-    #pragma parallel :N=>block(BN)
+def _torch_kernel(a, new_a, b, M, N):
+    #pragma parallel
     for i in range(M):  
         mean = sum(a[i,:N])
         mean /= N
-        a[i:N] -= mean
+        new_a[i,:N] = a[i,:N] - mean
 
-    #pragma parallel :N=>block(BN)
+    #pragma parallel
     for i in range(M):  
-        for j in range(i):  
-            b[i,j] = sum(a[i,:N] * a[j,:N])
+        for j in range(i, M):  
+            b[i,j] = sum(new_a[i,:N] * new_a[j,:N])
+            b[i,j] /= N - 1
+            b[j,i] = b[i,j]
+
+@jit
+def _mykernel(a, new_a, b, M, N, BN=512):
+    #pragma parallel
+    for i in range(M):  
+        mean = sum(a[i,:N])
+        mean /= N
+        new_a[i,:N] = a[i,:N] - mean
+
+    #pragma parallel
+    for i in range(M):  
+        for j in range(i, M):  
+            b[i,j] = sum(new_a[i,:N] * new_a[j,:N])
             b[i,j] /= N - 1
             b[j,i] = b[i,j]
             
-@jit
-def _mykernel_blocked(a, b, u, M, N, BM=8, BN=512):
-    #pragma parallel
-    for i in range(M):  
-        mean = sum(a[i,:N]) / N
-        a[i:N] -= mean
-
-    #pragma parallel
-    for i in range(0, M, BM):
-        #pragma parallel
-        for j in range(0, M, BM):  
-            b[i:i+BM,j:j+BM] = a[i:i+BM,:N] @ t(a[j:j+BM,:N])
-            b[i:i+BM,j:j+BM] /= N - 1
 
 def numba_kernel(a):
     n_vars, n_obs = a.shape
-    u = torch.zeros([n_vars], dtype=a.dtype)
-    b = torch.empty([n_vars, n_vars], dtype=a.dtype)
-    _numba_kernel(a.cpu().numpy(), b.cpu().numpy(), u.cpu().numpy(), n_vars, n_obs)
+    new_a = np.empty_like(a)
+    b = np.empty([n_vars, n_vars], dtype=a.dtype)
+    _numba_kernel(a, new_a, b, n_vars, n_obs)
     return b
 
 @numba.jit(nopython=True, parallel=True, cache=True)
-def _numba_kernel(a, b, u, M, N):
+def _numba_kernel(a, new_a, b, M, N):
     for i in numba.prange(M):  
         mean = np.sum(a[i,:N])
         mean /= N
-        a[i:N] -= mean
+        new_a[i,:N] = a[i,:N] - mean
 
     for i in numba.prange(M):  
-        for j in range(i):  
-            b[i,j] = np.sum(a[i,:N] * a[j,:N])
+        for j in range(i, M):  
+            b[i,j] = np.sum(new_a[i,:N] * new_a[j,:N])
             b[i,j] /= N - 1
             b[j,i] = b[i,j]
 
-def _torch_kernel(a, b, u, M, N, BN=512):
-    #pragma parallel :N=>block(BN)
-    for i in range(M):  
-        mean = sum(a[i,:N])
-        mean /= N
-        a[i:N] -= mean
-
-    #pragma parallel :N=>block(BN)
-    for i in range(M):  
-        for j in range(i):  
-            b[i,j] = sum(a[i,:N] * a[j,:N])
-            b[i,j] /= N - 1
-            b[j,i] = b[i,j]
 
 def test1():
     for dtype in [torch.float32]:
     #for dtype in [torch.float64]:
-        for M, N in [(1024, 1024), (1024*4, 1024*4), (1024*16, 1024*16)]:
+        #for M, N in [(1024, 1024), (1024*4, 1024*4), (1024*16, 1024*16)]:
+        for M, N in [(512, 512), (1024, 1024)]:
         #for M, N in [(1024, 256*2), (4096, 4096), (4096*4, 4096*4), (4096, 4096*8), (4096, 4096*16), (128, 4096*16), (256, 4096*16)]:
         #for M, N in [(8, 256)]:
             print(f'M: {M}, N: {N}')
-            a = torch.randn(M, N, dtype=dtype)
-            b_ref = torch_kernel(a)
+            a = torch.randn(M, N, dtype=dtype).to('cuda')
+            b_ref = mykernel(a, _torch_kernel)
 
-            for f in (torch_kernel, _mykernel_2_loops, _mykernel_3_loops,  _mykernel_blocked, _mykernel_ops):
-            #for f in (torch_kernel, _mykernel_2_loops):
-                ff = lambda: f(a)
-                if f.__name__.startswith('_'):
-                    ff = lambda: mykernel(a, f)
+            for f in (_mykernel, numba_kernel, _torch_kernel):
+                if f.__name__.startswith('numba'):
+                    a_np = a.cpu().numpy()
+                    ff = lambda: f(a_np)
+                else:
+                    if f.__name__.startswith('_'):
+                        ff = lambda: mykernel(a, f)
+                    else:
+                        ff = lambda: f(a)
+                    
                 b = ff()
-                if not torch.allclose(b, b_ref, atol=0.1, rtol=0.01):
+                if isinstance(b, np.ndarray):
+                    b = torch.from_numpy(b).to('cuda')
+
+                if not torch.allclose(b, b_ref, atol=0.1, rtol=0.05):
                     print(b)
                     print(b_ref)
-                assert(torch.allclose(b, b_ref, atol=0.1, rtol=0.01))
+                assert(torch.allclose(b, b_ref, atol=0.1, rtol=0.05))
                 #ms, _, _ = triton.testing.do_bench(ff)
                 ms = bench(ff)
                 print(f'{f.__name__}: {ms:.4f} ms')
--- slap/utils.py
+++ slap/utils.py
@@ -8,7 +8,13 @@ def bench(fn):
         globals={'fn': fn},
         num_threads=torch.get_num_threads()
     )
-    return t0.timeit(20).mean * 1000
+    N = 20
+    one_run = t0.timeit(1).mean
+    if one_run > 1:
+        N = 2
+    elif one_run > 5:
+        N = 1
+    return t0.timeit(N).mean * 1000
 
 def allclose(a, b):
     if isinstance(a, torch.Tensor):
