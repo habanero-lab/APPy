2bc7bd31407a98911209dfe2edc7b96b854ff2d7
Tong Zhou <tz@gatech.edu>
2023-08-23 14:37:23 -0400

    fixed a number of issues, now more benchmarks are good using extracted args, BLOCK args are rearranged to the last because they require tl.constexpr

--- appy/codegen/high_level_transforms/get_loaded_names.py
+++ appy/codegen/high_level_transforms/get_loaded_names.py
@@ -10,26 +10,29 @@ class GetLoadedNames(ast.NodeVisitor):
         self.stored_vars = []
 
     def visit_Subscript(self, node):
-        if isinstance(node.value, ast.Name):
-            
+        if isinstance(node.value, ast.Name):            
             ndim = 1
             if isinstance(node.slice, ast.Tuple):
                 ndim = len(node.slice.elts)
-            self.names[node.value.id] = ndim
+            self.names[node.value.id] = ('tensor', ndim)
             print(self.names)
-        else:
-            self.generic_visit(node)
+        self.generic_visit(node)
 
+    def visit_Call(self, node: ast.Call):
+        if unparse(node).startswith('vidx(') or unparse(node).startswith('appy.vidx('):
+            if isinstance(node.args[1], ast.Name):
+                self.names[node.args[1].id] = ('const', 0)
+        self.generic_visit(node)
 
     def visit_Name(self, node):        
         if node.id.startswith('_top_var'):
             return
-
-        if node.id in ['range', 'vidx', 'APPY_BLOCK', 'float', 'int', 'tl', 'torch']:
+        
+        if node.id in ['range', 'vidx', 'float', 'int', 'tl', 'torch']:
             return
 
         if isinstance(node.ctx, ast.Store):
-            self.stored_vars.append(node.id) 
+            self.stored_vars.append(node.id)         
         
         if node.id not in self.stored_vars and (node.id not in self.names):
-            self.names[node.id] = 0
+            self.names[node.id] = ('scalar', 0)
--- appy/codegen/rewrite_pfor.py
+++ appy/codegen/rewrite_pfor.py
@@ -4,6 +4,7 @@ from ast import unparse
 from appy.ast_utils import *
 from copy import deepcopy
 import appy.codegen.typesys as typesys
+from collections import OrderedDict
 
 class RewritePFor(ast.NodeTransformer):
     def __init__(self, module, options, arg_type_map):
@@ -15,7 +16,7 @@ class RewritePFor(ast.NodeTransformer):
     def create_new_kernel_function(self):
         kernel_name = f'_kernel{self.kernel_count}'
         self.kernel_count += 1
-        k_params = self.get_kernel_function_parameters(self.extracted_args)
+        k_params = self.make_kernel_parameters(self.extracted_args)
         kf = ast.parse(textwrap.dedent(f'''
             @triton.jit
             def {kernel_name}({', '.join(k_params)}):
@@ -24,20 +25,23 @@ class RewritePFor(ast.NodeTransformer):
         self.kf = kf
         return kf
 
-    def get_kernel_function_parameters(self, arg_dim_map):
+    def make_kernel_parameters(self, arg_dim_map):
         newargs = []
-        for name, ndim in arg_dim_map.items():
-            #print(name, val)
-            newargs.append(name)
+        for name, (ty, ndim) in arg_dim_map.items():        
+            if ty == 'const':
+                newargs.append(f'{name}: tl.constexpr')
+            else:
+                newargs.append(name)
+
             if ndim > 0:
                 for d in range(ndim):
                     #newargs.append(f'{name}_shape_{d}')
-                    newargs.append(f'{name}_stride_{d}')
+                    newargs.append(f'{name}_stride_{d}')        
         return newargs
 
-    def get_kernel_function_arguments(self, arg_dim_map):
+    def make_kernel_actual_arguments(self, arg_dim_map):
         newargs = []
-        for name, ndim in arg_dim_map.items():
+        for name, (ty, ndim) in arg_dim_map.items():
             #print(name, val)
             newargs.append(name)
             if ndim > 0:
@@ -79,9 +83,13 @@ class RewritePFor(ast.NodeTransformer):
 
     def make_triton_configs(self, configs):    
         keys = []
-        for param in self.get_kernel_function_parameters(self.extracted_args):
-            if '_shape_' in param:
-                keys.append('"' + param.replace(': tl.constexpr', '') + '"')
+        for name, (ty, ndim) in self.extracted_args.items():
+            if ty == 'tensor':
+                for d in range(ndim):
+                    keys.append(f'"{name}_stride_{d}"') 
+        # for param in self.make_kernel_parameters(self.extracted_args):
+        #     if '_shape_' in param:
+        #         keys.append('"' + param.replace(': tl.constexpr', '') + '"')
         
         triton_configs = []
         for config in configs:
@@ -113,25 +121,34 @@ class RewritePFor(ast.NodeTransformer):
             from .high_level_transforms.get_loaded_names import GetLoadedNames
             self.extracted_args = {}
             GetLoadedNames(self.extracted_args).visit(node)
+            # Rearrange the args to make tl.constexpr args the last
+            reordered = OrderedDict()
+            for name, (ty, ndim) in self.extracted_args.items():
+                if ty == 'const':
+                    continue
+                else:
+                    reordered[name] = (ty, ndim)
 
-            kf = self.create_new_kernel_function()
+            for name, (ty, ndim) in self.extracted_args.items():
+                if ty == 'const':
+                    reordered[name] = (ty, ndim)
+            self.extracted_args = reordered
             #exit(1)
+                    
 
             from appy.codegen.triton_transformer import TritonKernelTransformer
             grid = []
             kernel_code = TritonKernelTransformer(grid).visit(node)                 
+            kf = self.create_new_kernel_function()
             kf.body += kernel_code
             meta_grid = f'kernel_grid = lambda META: ({",".join(grid)},)'                
             #print(meta_grid)
             #print(self.options)
-              
-                
-            k_args = self.get_kernel_function_arguments(self.extracted_args)
+                              
+            k_args = self.make_kernel_actual_arguments(self.extracted_args)
             if self.options.get('tune'):                    
                 configs = []
-                for key, values in self.options.get('tune').items():
-                    if key == 'APPY_BLOCK':
-                        append_new_argument(kf, key, annotation='tl.constexpr')                            
+                for key, values in self.options.get('tune').items():                          
                         
                     for value in values:
                         configs.append({key: value})
