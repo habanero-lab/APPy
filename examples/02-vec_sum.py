3d289df6a9052a91b0aace9d7b35c26a8abd5165
Tong <tz@gatech.edu>
2024-02-22 09:37:29 -0500

    added numbers to the examples

---
+++ examples/02-vec_sum.py
@@ -0,0 +1,36 @@
+import appy
+
+# APPy tensors are `torch` tensors by default. Uncomment the 
+# following lines to make APPy work with (and return) `cupy` tensors.
+#import cupy
+#appy.tensorlib = cupy
+
+@appy.jit  # Comment this line to run the function in the Python interpter (debug mode)
+def kernel_appy(a):
+    ## Zero-initialize the output array
+    b = appy.zeros(1, dtype=a.dtype)
+    N = a.shape[0]
+    #pragma parallel for simd
+    for i in range(N): 
+        #pragma atomic
+        b[0] += a[i]
+    return b
+
+
+def kernel_lib(a):
+    return a.sum()
+
+
+def test():
+    for N in [10000, 100000, 1000000, 10000000]:
+        a = appy.randn(N, dtype='float32')
+        c_ref = kernel_lib(a)
+        print(f"N: {a.shape[0]}, dtype: {a.dtype}")
+        for f in [kernel_lib, kernel_appy]:
+            c = f(a)
+            assert appy.utils.allclose(c, c_ref, atol=1e-6)
+            ms = appy.utils.bench(lambda: f(a))
+            print(f"{f.__name__}: {ms:.4f} ms")
+
+if __name__ == '__main__':
+    test()
---
+++ examples/04-jacobi_2d.py
@@ -0,0 +1,43 @@
+import appy
+import copy
+
+
+@appy.jit(auto_simd=True)
+def kernel_appy(A, B):
+    M, N = A.shape
+    for t in range(1, 10):
+        #pragma 1:M-1=>parallel 1:N-1=>parallel
+        B[1:M-1, 1:N-1] = 0.2 * (A[1:M-1, 1:N-1] + A[1:M-1, :N-2] + A[1:M-1, 2:N] +
+                                A[2:M, 1:N-1] + A[0:M-2, 1:N-1])
+        #pragma 1:M-1=>parallel 1:N-1=>parallel
+        A[1:M-1, 1:N-1] = 0.2 * (B[1:M-1, 1:N-1] + B[1:M-1, :N-2] + B[1:M-1, 2:N] +
+                                B[2:M, 1:N-1] + B[0:M-2, 1:N-1])
+    return A, B
+
+
+def kernel_lib(A, B):
+    for t in range(1, 10):        
+        B[1:-1, 1:-1] = 0.2 * (A[1:-1, 1:-1] + A[1:-1, :-2] + A[1:-1, 2:] +
+                               A[2:, 1:-1] + A[:-2, 1:-1])
+        A[1:-1, 1:-1] = 0.2 * (B[1:-1, 1:-1] + B[1:-1, :-2] + B[1:-1, 2:] +
+                               B[2:, 1:-1] + B[:-2, 1:-1])
+    return A, B
+
+
+def test():
+    for N in [1000, 4000]:
+        A = appy.randn(N, N)
+        B = appy.randn(N, N)
+        A_ref, B_ref = kernel_lib(copy.deepcopy(A), copy.deepcopy(B))
+        print(f"shape: {A.shape}, dtype: {A.dtype}")
+        for f in [kernel_lib, kernel_appy]:
+            A_, B_ = f(copy.deepcopy(A), copy.deepcopy(B))
+            assert appy.utils.allclose(A_, A_ref, atol=1e-6)
+            #A_copy, B_copy = copy.deepcopy(A), copy.deepcopy(B)
+            #ms = appy.utils.bench(lambda: f(A_copy, B_copy))
+            ms = appy.utils.bench(lambda: f(copy.deepcopy(A), copy.deepcopy(B)))
+            print(f"{f.__name__}: {ms:.4f} ms")
+
+
+if __name__ == "__main__":
+    test()
