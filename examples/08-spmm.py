b41c5db54bd4727c7602bba95dd5da6f16a90ecb
Tong <tz@gatech.edu>
2024-03-13 01:16:35 -0400

    updated 08-spmm

--- examples/08-spmm.py
+++ examples/08-spmm.py
@@ -1,36 +1,17 @@
 import appy
-import cupy
 import cupyx
 import torch
 
 
-@appy.jit(auto_simd=True, dump_final_appy=True)
-def kernel_appy0(A_data, A_indptr, A_indices, B, N, K):
-    y = appy.empty([N, K], dtype=B.dtype)
-    #pragma parallel for
-    for i in range(N):
-        start, end = A_indptr[i], A_indptr[i+1]
-        y[i, :K] = 0
-        for j in range(start, end):
-            y[i, :K] += A_data[j] * B[A_indices[j], :K]
-    return y
-
-
 @appy.jit(auto_simd=True)
-def kernel_appy(A_data, A_indptr, A_indices, B, N, K):
-    y = appy.empty([N, K], dtype=B.dtype)
+def kernel_appy(A_data, A_indptr, A_indices, B, M, N):
+    y = appy.empty([M, N], dtype=B.dtype)
     #pragma parallel for
-    for i in range(N):
+    for i in range(M):
         start, end = A_indptr[i], A_indptr[i+1]
-        #pragma :K=>le(128)
-        y[i, :K] = 0.0
-        #pragma :K=>le(128)
-        acc = y[i, :K]
-        for j in range(start, end):
-            #pragma :K=>le(128)
-            acc += A_data[j] * B[A_indices[j], :K]
-        #pragma :K=>le(128)
-        y[i, :K] = acc
+        y[i, :N] = 0
+        for k in range(start, end):
+            y[i, :N] += A_data[k] * B[A_indices[k], :N]
     return y
 
 
@@ -39,21 +20,20 @@ def kernel_lib(A, B):
 
 
 def test():
-    N = 20000
-    K = 200
+    M = K = 20000
+    N = 200
     for sparsity in [0.0001, 0.0004, 0.001, 0.004, 0.01, 0.04, 0.1]:    
-        A = cupyx.scipy.sparse.random(N, N, sparsity).tocsr()
-        B = cupy.random.randn(N, K)
+        A = cupyx.scipy.sparse.random(M, K, sparsity).tocsr()
         print(f'A shape: {A.shape}, sparsity: {sparsity} A nnz: {A.count_nonzero()}')
         
-        A = torch.sparse_csr_tensor(A.indptr, A.indices, A.data, size=(N, N), device='cuda')
-        B = torch.as_tensor(B, device='cuda')
+        A = torch.sparse_csr_tensor(A.indptr, A.indices, A.data, size=(M, K), device='cuda')
+        B = torch.randn(K, N, dtype=A.dtype, device='cuda')
         
         y_ref = kernel_lib(A, B)
-        y = kernel_appy0(A.values(), A.crow_indices(), A.col_indices(), B, N, K)
+        y = kernel_appy(A.values(), A.crow_indices(), A.col_indices(), B, M, N)
         assert appy.utils.allclose(y, y_ref, atol=1e-6)
         ms0 = appy.utils.bench(lambda: kernel_lib(A, B))
-        ms1 = appy.utils.bench(lambda: kernel_appy0(A.values(), A.crow_indices(), A.col_indices(), B, N, K))
+        ms1 = appy.utils.bench(lambda: kernel_appy(A.values(), A.crow_indices(), A.col_indices(), B, M, N))
         print(f"kernel_lib: {ms0:.4f} ms")
         print(f"kernel_appy: {ms1:.4f} ms")
         print(f'speedup over lib: {(ms0/ms1):.3f}')
