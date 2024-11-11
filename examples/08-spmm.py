4612b04d1bcbfb512433e8b4d1e9af3da02295d7
Tong <tz@gatech.edu>
2024-11-10 21:44:35 -0500

    updated 08-spmm a bit

--- examples/08-spmm.py
+++ examples/08-spmm.py
@@ -21,11 +21,13 @@ def kernel_lib(A, B):
 
 def test():
     M = K = 20000
-    N = 200
-    for sparsity in [0.0001, 0.0004, 0.001, 0.004, 0.01, 0.04, 0.1]:    
+    N = 256
+    print(f'Computing SpMM: A ({M}x{K}, sparse) @ B ({K}x{N}, dense)')
+    #for sparsity in [0.0001, 0.0004, 0.001, 0.004, 0.01, 0.04, 0.1]:    
+    for sparsity in [0.0001, 0.0004, 0.001, 0.01]:    
         A = cupyx.scipy.sparse.random(M, K, sparsity).tocsr()
-        print(f'A shape: {A.shape}, sparsity: {sparsity} A nnz: {A.count_nonzero()}')
-        
+        print(f'A sparsity: {sparsity}, nnz: {A.count_nonzero()}')
+        #print(f'B shape: {B.shape}')
         A = torch.sparse_csr_tensor(A.indptr, A.indices, A.data, size=(M, K), device='cuda')
         B = torch.randn(K, N, dtype=A.dtype, device='cuda')
         
@@ -40,7 +42,7 @@ def test():
 
         A = A.to_dense()
         ms2 = appy.utils.bench(lambda: kernel_lib(A, B))
-        print(f"kernel_lib (dense): {ms2:.2f} ms")
+        print(f"kernel_lib (dense): {ms2:.2f} ms\n")
         # A, B = A.cpu(), B.cpu()
         # ms2 = appy.utils.bench(lambda: kernel_lib(A, B))
         # print(f"kernel_lib (CPU): {ms2:.4f} ms")
