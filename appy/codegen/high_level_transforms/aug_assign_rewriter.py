31d85846191a238516ca309f7d2e8457cf974b8a
Tong <tz@gatech.edu>
2025-04-05 11:13:25 -0400

    fix a minor bug

--- appy/codegen/high_level_transforms/aug_assign_rewriter.py
+++ appy/codegen/high_level_transforms/aug_assign_rewriter.py
@@ -1,17 +1,23 @@
 import ast_comments as ast
-from ast import unparse
-from appy.ast_utils import *
-from copy import deepcopy
+from ast_transforms.utils import *
 
 class RewriteAugAssign(ast.NodeTransformer):
+    '''
+    This class rewrites AugAssign nodes to Assign nodes, e.g.
+    x += 1 is rewritten to x = x + 1
+    '''
     def visit_AugAssign(self, node):
         left = node.target
         right = node.value
-        newnode = ast.Assign(targets=[left], lineno=node.lineno)
-        leftcopy = deepcopy(left)
-        if isinstance(leftcopy, ast.Subscript):
-            leftcopy.ctx = ast.Load()
-        newnode.value = ast.BinOp(left=leftcopy, op=node.op, right=node.value)
+        newnode = ast.Assign(
+                targets=[left], 
+                value=ast.BinOp(
+                    left=deepcopy_ast_node(left, ctx=ast.Load()), 
+                    op=node.op, 
+                    right=right
+                ),
+                lineno=node.lineno,
+            )
 
         if isinstance(node.op, ast.Add):
             newnode.reduce = '+'
