da5f1f63c2f1e35e870669cfa3c5c19b2f8ba790
Tong Zhou <tz@gatech.edu>
2025-04-14 14:24:54 -0400

    added clause to and from

--- appy/codegen/high_level_transforms/process_data_pragma.py
+++ appy/codegen/high_level_transforms/process_data_pragma.py
@@ -25,21 +25,34 @@ class ProcessDataPragma(ast.NodeTransformer):
         self.generic_visit(node)
         if hasattr(node, 'pragma_dict'):
             d = node.pragma_dict
+            to_device_stmts = []
+            from_device_stmts = []
             if d.get('shared', None):
                 global_scalars = d['shared']
                 global_scalars = [global_scalars] if type(global_scalars) == str else global_scalars
-                to_gpu_stmts = []
+                
                 for scalar in global_scalars:
-                    to_gpu_stmts.append(
+                    to_device_stmts.append(
                         to_ast_node(f'{scalar} = torch.tensor([{scalar}], device="cuda")')
                     )
-                from_gpu_stmts = []
-                for scalar in global_scalars:
-                    from_gpu_stmts.append(
+                    from_device_stmts.append(
                         to_ast_node(f'{scalar} = {scalar}.cpu().item()')
                     )
-                # Inside the for loop, replace all occurences of the global scalar `x` with `x[0]`
+                # Inside the for loop, replace all occurences of the shared scalar `x` with `x[0]`
                 ReplaceScalars(global_scalars).visit(node)
-                return to_gpu_stmts + [node] + from_gpu_stmts
+            
+            if d.get('to', None):
+                for arr in [d['to']] if type(d['to']) == str else d['to']:
+                    to_device_stmts.append(
+                        to_ast_node(f'{arr} = torch.from_numpy({arr}).to("cuda")')
+                    )
+
+            if d.get('from', None):
+                for arr in [d['from']] if type(d['from']) == str else d['from']:
+                    from_device_stmts.append(
+                        to_ast_node(f'{arr} = {arr}.cpu().numpy()')
+                    )
+
+            return to_device_stmts + [node] + from_device_stmts
         
         return node
