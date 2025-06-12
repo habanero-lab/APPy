f476644d2d08e216c7a71ffbb7e45ef92227ce35
Tong <tz@gatech.edu>
2025-06-12 10:12:57 -0400

    added Jun's approach on reduction detection

---
+++ appy/codegen/high_level_transforms/mark_reduction_stmts_new.py
@@ -0,0 +1,76 @@
+'''
+This pass marks reduction statements as such by adding the `reduce` attribute.
+A statement is determined to be a reduction if 
+
+1. It has form `x = x + y` or `x = x * y` or `x = max(x, y)` or `x = min(x, y)`
+2. The statement is the only definition of `x` inside a loop
+3. The defining loop contains only a straight line of statements (i.e., no control flow)
+'''
+
+import ast
+from appy.ast_utils import *
+
+
+class GetAssignReductionOps(ast.NodeVisitor):
+    def __init__(self):
+        self.reduction_ops = {}
+
+    def get_reduction_op(self, node):
+        '''
+        Return the reduction operator for `node` if `node` is a reduction candidate, otherwise return None
+        '''
+        assert isinstance(node, ast.Assign)
+        target = node.targets[0]
+        value = node.value
+        if isinstance(target, ast.Name):
+            if isinstance(value, ast.BinOp):
+                if isinstance(value.op, ast.Add) or isinstance(value.op, ast.Mult):
+                    op = '+' if isinstance(value.op, ast.Add) else '*'
+                    if isinstance(value.left, ast.Name) and target.id == value.left.id:
+                        return op
+                    elif isinstance(value.right, ast.Name) and target.id == value.right.id:
+                        return op
+            elif isinstance(value, ast.Call):
+                if value.func.id == 'max' or value.func.id == 'min':
+                    op = value.func.id
+                    if isinstance(value.args[0], ast.Name) and target.id == value.args[0].id:
+                        return op
+                    elif isinstance(value.args[1], ast.Name) and target.id == value.args[1].id:
+                        return op
+
+        return None
+
+    def visit_Assign(self, node):
+        target = node.targets[0]
+        if isinstance(target, ast.Name):
+            op = self.get_reduction_op(node)
+            self.reduction_ops.get(target.id, set()).add(op)
+        return node
+
+
+class MarkReductionStmts(ast.NodeTransformer):
+    def __init__(self, reduction_ops):
+        self.reduction_ops = reduction_ops
+
+    def visit_Assign(self, node):
+        target = node.targets[0]
+        if isinstance(target, ast.Name):
+            if target.id in self.reduction_ops and len(self.reduction_ops[target.id]) == 1:
+                node.reduce = next(iter(self.reduction_ops[target.id]))
+        return node
+
+
+class MarkReductionStmtsInLoops(ast.NodeTransformer):
+    def visit_For(self, node):
+        self.generic_visit(node)
+        # Perform reduction analysis: if all assignments to a scalar variable
+        # only ever had one type of reduction ops, mark it as a reduction. 
+        # For example, if it has both None and +, then it is not a reduction.
+        visitor = GetAssignReductionOps()
+        visitor.visit(node)
+        reduction_ops = visitor.reduction_ops
+        return MarkReductionStmts(reduction_ops).visit(node)
+    
+
+def transform(tree):
+    return MarkReductionStmtsInLoops().visit(tree)
