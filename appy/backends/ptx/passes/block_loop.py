210c18ea95f932449aaaa35e3cd546f5cc61b330
Tong Zhou <zt9465@gmail.com>
2025-10-23 22:22:50 +0800

    fix: fix block_loop pass to replace old names with new names

--- appy/backends/ptx/passes/block_loop.py
+++ appy/backends/ptx/passes/block_loop.py
@@ -1,6 +1,10 @@
 import ast
+import ast_transforms as at
 
 class BlockLoop(ast.NodeTransformer):
+    def __init__(self):
+        self.name_map = {}
+
     def get_loop_pragma(self, node):
         return {"parallel": True}
     
@@ -10,12 +14,25 @@ class BlockLoop(ast.NodeTransformer):
             # Rewrite the loop index to __ctaid_x and insert 
             # necessary assignments in the beginning of the loop body.
             # Note: for nested prange loops, we'd use __ctaid_y, __ctaid_z, etc.
-            # But that is not handled here for simplicity.
-            simt_index = ast.Name(id="__ctaid_x", ctx=ast.Load())
-            assign = ast.Assign(targets=[node.target], value=simt_index)
-            node.body.insert(0, assign)
-            node.target = simt_index
+            # But that is not handled here for simplicity.            
+            self.name_map[node.target.id] = "__ctaid_x"
+            assign = ast.Assign(
+                targets=[ast.Name(id=node.target.id, ctx=ast.Store())], 
+                value=ast.Name(id="__ctaid_x", ctx=ast.Load())
+            )
+            node.body.insert(0, assign)            
+            node.target = ast.Name(id="__ctaid_x", ctx=ast.Store())            
+        else:
+            raise NotImplementedError("SIMD loops not implemented in BlockLoop pass.")
+        
         self.generic_visit(node)
         ast.fix_missing_locations(node)
+        return node
+    
+    def visit_Name(self, node):
+        if node.id in self.name_map:
+            new_node = ast.Name(id=self.name_map[node.id], ctx=node.ctx)
+            ast.copy_location(new_node, node)
+            return new_node
         return node
 
