a4cefe50ee16eb2cfdb3b870f63becf9f26c7ce8
Tong Zhou <zt9465@gmail.com>
2025-11-09 22:21:23 +0800

    fix: fix triton backend to use the latest backend interface

--- appy/backends/triton/backend.py
+++ appy/backends/triton/backend.py
@@ -6,23 +6,17 @@ import ast_transforms as at
 from ..base import Backend
 
 class TritonBackend(Backend):
-    def codegen(self, loop_source, metadata):
-        tree = ast.parse(loop_source).body[0]
-        used_names = at.get_used_names(tree, no_funcname=True)
-        # Remove loop target name from used_names since it should be a local var regardless
-        used_names = [x for x in used_names if x != tree.target.id]
-        
-        vec_add = tw.dedent('''
-        for i in appy.prange(a.shape[0]):
+    def __init__(self, val_map=None):
+        self.val_map = val_map or {}   
+
+    def codegen(self, tree, metadata):
+        vec_add = ast.parse(tw.dedent('''
+        for i in appy.prange(a_shape_0):
             c[i] = a[i] + b[i]
-        ''').strip()
+        ''').strip())
 
-        sample_kernels = {
-            vec_add: "vec_add"
-        }
-
-        if loop_source in sample_kernels:
-            m = Path(f"{os.environ["HOME"]}/projects/APPy/appy/backends/triton/sample_kernels/{sample_kernels[loop_source]}.py").read_text()
+        if ast.dump(tree) == ast.dump(vec_add):
+            m = Path(f"{os.environ["HOME"]}/projects/APPy/appy/backends/triton/sample_kernels/vec_add.py").read_text()
             return m
         else:
             raise NotImplementedError()
