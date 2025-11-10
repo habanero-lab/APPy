8b50cdca68aef5c1b363195aa66dabfe7ea9ab49
Tong Zhou <zt9465@gmail.com>
2025-11-10 15:56:38 +0800

    feat: add triton pass rewrite_range.py; remove an unused package in the old code

--- appy/backends/triton/passes/rewrite_range.py
+++ appy/backends/triton/passes/rewrite_range.py
@@ -1,6 +1,4 @@
-import ast_comments as ast
-from ast import unparse
-from appy.ast_utils import *
+import ast
 
 class RewriteRange(ast.NodeTransformer):
     def visit_Call(self, node):
@@ -8,17 +6,20 @@ class RewriteRange(ast.NodeTransformer):
             if len(node.args) == 1:
                 up = node.args[0]
                 node.args = [
-                    new_const_node(0),
+                    ast.Constant(0),
                     up,
-                    new_const_node(1)
+                    ast.Constant(1)
                 ]
             elif len(node.args) == 2:
                 low, up = node.args
                 node.args = [
                     low,
                     up,
-                    new_const_node(1)
+                    ast.Constant(1)
                 ]
 
             assert len(node.args) == 3, ast.dump(node)
         return node
+    
+def transform(node):
+    return RewriteRange().visit(node)
