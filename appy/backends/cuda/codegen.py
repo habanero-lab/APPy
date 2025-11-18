e928b42222a840eb30c5e81b5221a330704f9e5f
Tong Zhou <zt9465@gmail.com>
2025-11-18 16:37:28 +0800

    refactor: update cuda codegen to also use the pragma-based model

--- appy/backends/cuda/codegen.py
+++ appy/backends/cuda/codegen.py
@@ -1,5 +1,5 @@
 import os
-import ast
+import ast_comments as astc
 import textwrap as tw
 from pathlib import Path
 
@@ -16,14 +16,20 @@ def codegen(tree, loop_name, val_map, options):
         c[i] = a[i] + b[i]
     ''').strip()
 
+    vec_add_v2 = tw.dedent('''
+    #pragma parallel for simd
+    for i in range(0, a_shape_0, 1):
+        c[i] = a[i] + b[i]
+    ''').strip()
+
     kernel_map = {
         vec_add_v0: "vec_add.py",
-        vec_add_v1: "vec_add.py"
+        vec_add_v1: "vec_add.py",
+        vec_add_v2: "vec_add.py",
     }
 
-    print(ast.unparse(tree))
-    if ast.unparse(tree) in kernel_map:
-        m = Path(f"{os.environ["HOME"]}/projects/APPy/appy/backends/cuda/sample_kernels/{kernel_map[ast.unparse(tree)]}").read_text()
+    if astc.unparse(tree) in kernel_map:
+        m = Path(f"{os.environ["HOME"]}/projects/APPy/appy/backends/cuda/sample_kernels/{kernel_map[astc.unparse(tree)]}").read_text()
         return m
     else:
         raise NotImplementedError()
