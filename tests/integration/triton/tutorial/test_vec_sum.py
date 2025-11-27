e5d39d2cc341295d0af04960e3f3d512c783b8e9
Tong Zhou <zt9465@gmail.com>
2025-11-27 23:56:12 +0800

    test: add test_max

--- tests/integration/triton/tutorial/test_vec_sum.py
+++ tests/integration/triton/tutorial/test_vec_sum.py
@@ -1,7 +1,7 @@
 import numpy as np
 import appy
 
-@appy.jit(backend="triton", dump_code=True)
+@appy.jit(backend="triton", dump_code=True, dry_run=0)
 def vec_sum(a):
     sum = 0.0
     #pragma parallel for simd shared(sum)
@@ -9,8 +9,22 @@ def vec_sum(a):
         sum += a[i]
     return sum
 
-def test():
+def test_sum():
     n = 1000
     a = np.random.rand(n)
     b = vec_sum(a)
     assert np.allclose(b, a.sum())
+
+@appy.jit(backend="triton", dump_code=True, dry_run=0)
+def vec_max(a):
+    m = -np.inf
+    #pragma parallel for simd shared(m)
+    for i in range(a.shape[0]):
+        m = max(m, a[i])
+    return m
+
+def test_max():
+    n = 1000
+    a = np.random.rand(n)
+    b = vec_max(a)
+    assert np.allclose(b, a.max())
