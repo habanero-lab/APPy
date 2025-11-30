ac5e16ffe326daedf47f1d7d633db4197871ccb1
Tong Zhou <zt9465@gmail.com>
2025-11-30 14:22:37 +0800

    test: add test_rowwise_reduction

---
+++ tests/integration/triton/tutorial/test_rowwise_reduction.py
@@ -0,0 +1,53 @@
+import numpy as np
+import appy
+
+@appy.jit(backend="triton", dump_code=True)
+def matrix_rowwise_sum(a, b):
+    #pragma parallel for
+    for i in range(a.shape[0]):
+        #pragma simd
+        s = 0.0
+        for j in range(a.shape[1]):
+            s += a[i, j]
+        b[i] = s
+
+def test_rowwise_sum():
+    a = np.random.rand(100, 100)
+    b = np.empty(100)
+    matrix_rowwise_sum(a, b)
+    assert np.allclose(b, a.sum(axis=1))
+
+@appy.jit(backend="triton", dump_code=True)
+def matrix_rowwise_max(a, b):
+    #pragma parallel for
+    for i in range(a.shape[0]):
+        
+        s = float("-inf")
+        #pragma simd
+        for j in range(a.shape[1]):
+            s = max(s, a[i, j])
+        b[i] = s
+
+def test_rowwise_max():
+    a = np.random.rand(100, 100)
+    b = np.empty(100)
+    matrix_rowwise_max(a, b)
+    assert np.allclose(b, a.max(axis=1))
+
+
+@appy.jit(backend="triton", dump_code=True)
+def matrix_rowwise_min(a, b):
+    #pragma parallel for
+    for i in range(a.shape[0]):
+        
+        s = float("inf")
+        #pragma simd
+        for j in range(a.shape[1]):
+            s = min(s, a[i, j])
+        b[i] = s
+
+def test_rowwise_min():
+    a = np.random.rand(100, 100)
+    b = np.empty(100)
+    matrix_rowwise_min(a, b)
+    assert np.allclose(b, a.min(axis=1))
