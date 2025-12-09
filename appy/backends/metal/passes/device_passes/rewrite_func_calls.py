bfc7c64265ba945527f3d5b1d7b7159e7fec3453
Tong Zhou <zt9465@gmail.com>
2025-12-09 21:59:09 +0800

    feat: update rewrite_func_calls.py to support Python built-in int() and float()

--- appy/backends/metal/passes/device_passes/rewrite_func_calls.py
+++ appy/backends/metal/passes/device_passes/rewrite_func_calls.py
@@ -2,6 +2,14 @@ import ast
 
 class RewriteFuncCalls(ast.NodeTransformer):
     def visit_Call(self, node):
+        # Handle special built-in functions
+        if isinstance(node.func, ast.Name):
+            if node.func.id in 'range':
+                return node
+            elif node.func.id in ['int', 'float']:
+                node.func = ast.Name(id=f'static_cast<{node.func.id}>', ctx=ast.Load())
+                return node
+
         from .. import metal_math
         funcname = None
         if isinstance(node.func, ast.Attribute):
@@ -9,8 +17,8 @@ class RewriteFuncCalls(ast.NodeTransformer):
         elif isinstance(node.func, ast.Name):
             funcname = node.func.id
 
-        if funcname in ['range', 'prange']:
-            return node
+        # if funcname in ['range', 'prange']:
+        #     return node
 
         assert hasattr(metal_math, funcname), "Unknown function: " + funcname
         node.func = ast.Name(id=funcname, ctx=ast.Load())
@@ -18,7 +26,10 @@ class RewriteFuncCalls(ast.NodeTransformer):
         return node
     
     def visit_For(self, node):
-        self.generic_visit(node)
+        new_body = []
+        for child in node.body:
+            new_body.append(self.visit(child))
+        node.body = new_body
         return node
     
 def transform(node):
