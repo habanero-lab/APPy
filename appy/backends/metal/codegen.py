b611ef6e50a263c0ff2230790b25688bb431835f
Tong Zhou <zt9465@gmail.com>
2025-12-10 06:53:25 +0800

    feat: add pass metal/passes/fix_float_div_types.py

--- appy/backends/metal/codegen.py
+++ appy/backends/metal/codegen.py
@@ -25,11 +25,13 @@ def codegen(loop_source, loop_name, val_map, options):
         # Metal specific codegen
         from .passes import rewrite_nested_prange
         from .passes import attach_types
+        from .passes import fix_float_div_types
         from .passes import gen_host_code
         from .passes import gen_device_code
 
         tree = rewrite_nested_prange.transform(tree)
         attach_types.visit(tree, val_map)
+        tree = fix_float_div_types.transform(tree)
 
         tree, replaced_loop = gen_host_code.transform(tree, {'loop_name': loop_name, 'val_map': val_map})
         tree = gen_device_code.transform(tree, replaced_loop, loop_name, val_map)
---
+++ appy/backends/metal/passes/fix_float_div_types.py
@@ -0,0 +1,30 @@
+import ast
+
+class FixFloatDivTypes(ast.NodeTransformer):
+    def visit_BinOp(self, node):
+        self.generic_visit(node)
+        if isinstance(node.op, ast.Div):
+            a, b = node.left, node.right
+            if a.appy_type == 'int' and b.appy_type == 'int':
+                node.left = ast.Call(func=ast.Name(id='float', ctx=ast.Load()), args=[a], keywords=[])
+                node.right = ast.Call(func=ast.Name(id='float', ctx=ast.Load()), args=[b], keywords=[])
+                node.left.appy_type = 'float'
+                node.right.appy_type = 'float'
+                node.appy_type = 'float'
+        return node
+    
+    def visit_Assign(self, node):
+        self.generic_visit(node)
+        if node.value.appy_type == 'float':
+            for target in node.targets:
+                target.appy_type = 'float'
+        return node
+    
+
+def transform(tree):
+    """
+    This pass detects division between two ints, and inserts a cast to float, e.g.
+    a / b is rewritten to float(a) / float(b). The return type is also updated to float.
+    """
+
+    return FixFloatDivTypes().visit(tree)
