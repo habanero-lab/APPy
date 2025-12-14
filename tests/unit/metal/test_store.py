572916a67f2433ee29f33314ad38db2e2ca6e6e3
Tong Zhou <zt9465@gmail.com>
2025-12-14 17:51:32 +0800

    test: update metal/test_store.py

--- tests/unit/metal/test_store.py
+++ tests/unit/metal/test_store.py
@@ -2,22 +2,42 @@ import appy.np_shared as nps
 import numpy as np
 import appy
 
-@appy.jit
-def store_int(out, alpha):
-    for i in range(out.shape[0]):
+@appy.jit(dump_code=True, clear_cache=True)
+def store_scalar(out, alpha):
+    for i in appy.prange(out.shape[0]):
         out[i] = alpha
 
-def test_store_int():
+def test_store_py_int():
     out = nps.empty((10,), dtype=np.int32)
-    store_int(out, 10)
+    store_scalar(out, 10)
     assert np.all(out == 10)
 
-@appy.jit
-def store_float(out, alpha):
-    for i in range(out.shape[0]):
-        out[i] = alpha
-
-def test_store_float():
+def test_store_py_float():
     out = nps.empty((10,), dtype=np.float32)
-    store_float(out, 3.3)
+    store_scalar(out, 3.3)
     assert np.all(out == 3.3)
+
+def test_store_int32():
+    out = nps.empty((10,), dtype=np.int32)
+    store_scalar(out, np.int32(10))
+    assert np.all(out == 10)
+
+def test_store_float32():
+    out = nps.empty((10,), dtype=np.float32)
+    store_scalar(out, np.float32(3.3))
+    assert np.all(out == 3.3)
+
+# def test_store_uint32():
+#     out = nps.empty((10,), dtype=np.uint32)
+#     store_scalar(out, np.uint32(10))
+#     assert np.all(out == 10)
+
+# def test_store_int8():
+#     out = nps.empty((10,), dtype=np.int8)
+#     store_scalar(out, np.int8(10))
+#     assert np.all(out == 10)
+
+def test_store_uint8():
+    out = nps.empty((10,), dtype=np.uint8)
+    store_scalar(out, np.uint8(10))
+    assert np.all(out == 10)
